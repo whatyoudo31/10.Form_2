835c4d1e777bf55f5ec85ec20453f40ccf15f210
whatyoudo31 <nikitofan@gmail.com>
2021-11-06 13:40:51 +0300

    123

--- src/app/layouts/oneOfUsers.jsx
+++ src/app/layouts/oneOfUsers.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import api from "../api";
-// import QualitiesList from "../components/qualitiesList";
+import QualitiesList from "../components/qualitiesList";
+import { Link } from "react-router-dom";
 
 const OneOfUsers = ({ userId }) => {
     const [user, setUser] = useState();
@@ -9,7 +10,23 @@ const OneOfUsers = ({ userId }) => {
         api.users.getById(userId).then((data) => setUser(data));
     }, []);
     console.log("ВЫБРАННЫЙ ЮЗЕР", user);
-    return user && <h1>{user.name}</h1>;
+    if (user) {
+        return (
+            <div>
+                <h1>{user.name}</h1>
+                <h3>Профессия: {user.profession.name}</h3>
+                <QualitiesList qualities={user.qualities} />
+                <p>Встретился {user.completedMeetings} раз</p>
+                <b>Оценка: {user.rate}</b>
+                <p>
+                    <Link to="/users">
+                        <button>Все пользователи</button>
+                    </Link>
+                </p>
+            </div>
+        );
+    }
+    return "Loading";
 };
 
 export default OneOfUsers;
